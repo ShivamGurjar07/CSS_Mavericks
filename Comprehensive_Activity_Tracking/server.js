db2b7cb6be900b75444e81aa4912c551df40fa9b
ShivamGurjar07 <gurjarcvam07@gmail.com>
2025-02-08 16:15:36 +0530

    add goal feature

--- Comprehensive_Activity_Tracking/server.js
+++ Comprehensive_Activity_Tracking/server.js
@@ -4,16 +4,19 @@ const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const activityRoutes = require("./routes/activityRoutes");
 const exportRoutes = require("./routes/exportRoutes");
+const goalRoutes = require("./routes/goalRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 connectDB();
-
+app.use(express.json());
 app.use(bodyParser.json());
 
 app.use("/api/activities", activityRoutes);
 app.use("/api/exports", exportRoutes);
+app.use("/api/goals", goalRoutes);
+
 
 
 app.listen(PORT, () => {
