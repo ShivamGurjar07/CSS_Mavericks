9589a6385a70b473577f6dbc9abc65d876944911
ShivamGurjar07 <gurjarcvam07@gmail.com>
2025-02-10 00:14:08 +0530

    add

--- Comprehensive_Activity_Tracking/api/server.js
+++ Comprehensive_Activity_Tracking/api/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const connectDB = require("./config/db");
-const activityRoutes = require("./routes/activityRoutes");
-const exportRoutes = require("./routes/exportRoutes");
-const goalRoutes = require("./routes/goalRoutes");
+const connectDB = require("../config/db");
+const activityRoutes = require("../routes/activityRoutes");
+const exportRoutes = require("../routes/exportRoutes");
+const goalRoutes = require("../routes/goalRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
